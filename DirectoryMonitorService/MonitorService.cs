04314baceb35dab38a3800c3ac8d07237c12db98
Mc Donald Fonyo <mcdonaldfonyo@outlook.com>
2020-06-24 16:47:45 +0200

    DB is updating

--- DirectoryMonitorService/MonitorService.cs
+++ DirectoryMonitorService/MonitorService.cs
@@ -259,7 +259,6 @@ namespace DirectoryMonitorService
                                 return false;
                             }
 
-
                         }
                         
                     }
